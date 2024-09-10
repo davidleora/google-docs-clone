13308988d88ec5ac7053e66710b692f5632c7883
liuleora@gmail.com <liuleora@gmail.com>
2024-09-10 11:59:22 +0900

    Updated classname for styling

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -10,5 +10,5 @@ export default function TextEditor() {
         wrapper.append(editor);
         new Quill(editor, { theme: "snow" });
     }, []);
-    return <div id="container" ref={wrapperRef}></div>;
+    return <div className="container" ref={wrapperRef}></div>;
 }
