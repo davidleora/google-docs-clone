ef13e53d42558599d34eadfdca66c2a3b0930e07
liuleora@gmail.com <liuleora@gmail.com>
2024-09-10 11:43:00 +0900

    Added wrapper to prevent multiple toolbars

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,14 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
 export default function TextEditor() {
-    const quillRef = useRef(null);
-
-    useEffect(() => {
-        if (!quillRef.current) {
-            quillRef.current = new Quill("#container", { theme: "snow" });
-        }
+    const wrapperRef = useCallback((wrapper) => {
+        if (!wrapper) return;
+        wrapper.innerHTML = "";
+        const editor = document.createElement("div");
+        wrapper.append(editor);
+        new Quill(editor, { theme: "snow" });
     }, []);
-    return <div id="container"></div>;
+    return <div id="container" ref={wrapperRef}></div>;
 }
