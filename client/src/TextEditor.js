bdff67b7d0f120a5ee7dd260853b4df9911bc0ab
liuleora@gmail.com <liuleora@gmail.com>
2024-09-10 11:35:44 +0900

    Create a basic text editor by using Quill

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,5 +1,14 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
 export default function TextEditor() {
-    return <div>Text Editor</div>;
+    const quillRef = useRef(null);
+
+    useEffect(() => {
+        if (!quillRef.current) {
+            quillRef.current = new Quill("#container", { theme: "snow" });
+        }
+    }, []);
+    return <div id="container"></div>;
 }
