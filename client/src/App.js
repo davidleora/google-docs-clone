a79e124b227477a01227252ef848346b0a43f118
liuleora@gmail.com <liuleora@gmail.com>
2024-09-10 13:30:10 +0900

    Added the save & load functions for different documents (using mongoDB)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import TextEditor from "./TextEditor";
+import {
+    BrowserRouter as Router,
+    Routes as Switch,
+    Route,
+    Navigate as Redirect,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-    return <TextEditor />;
+    return (
+        <Router>
+            <Switch>
+                <Route
+                    path="/"
+                    element={<Redirect to={`/documents/${uuidv4()}`} />}
+                />
+                <Route path="/documents/:id" element={<TextEditor />} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,7 +2,9 @@ import { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
+const SAVE_INTERVAL_MS = 2000;
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ font: [] }],
@@ -16,8 +18,12 @@ const TOOLBAR_OPTIONS = [
 ];
 
 export default function TextEditor() {
+    const { id: documentId } = useParams();
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
+
+    console.log(documentId);
+
     useEffect(() => {
         const s = io("http://localhost:3001");
         setSocket(s);
@@ -26,6 +32,29 @@ export default function TextEditor() {
             s.disconnect();
         };
     }, []);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        socket.once("load-document", (document) => {
+            quill.setContents(document);
+            quill.enable();
+        });
+
+        socket.emit("get-document", documentId);
+    }, [socket, quill, documentId]);
+
+    useEffect(() => {
+        if (socket == null || quill == null) return;
+
+        const interval = setInterval(() => {
+            socket.emit("save-document", quill.getContents());
+        }, SAVE_INTERVAL_MS);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [socket, quill]);
 
     useEffect(() => {
         if (socket == null || quill == null) return;
@@ -59,6 +88,8 @@ export default function TextEditor() {
             theme: "snow",
             modules: { toolbar: TOOLBAR_OPTIONS },
         });
+        q.disable();
+        q.setText("Loading...");
         setQuill(q);
     }, []);
     return <div className="container" ref={wrapperRef}></div>;
